4c1898703dafaed9546d72b74411bac82c078a5c
Sky3z <skyfallez4316@gmail.com>
2024-02-06 15:17:24 +0100

    first website view

--- home/views.py
+++ home/views.py
@@ -8,47 +8,20 @@ from django.conf import settings
 def HomePageView(request):
     return render(request, 'index.html')
 
-def CampingPageView(request):
-    return render(request, 'campeggio.html')
+def ServiziPageView(request):
+    return render(request, 'servizi.html')
 
-def WellnessPageView(request):
-    return render(request, 'wellness.html')
+def DintorniPageView(request):
+    return render(request, 'dintorni.html')
 
-def RistorantePageView(request):
-    return render(request, 'ristorante.html')
-
-def SistemazioniPageView(request):
-    return render(request, 'sistemazioni.html')
-
-def PiazzolebasicPageView(request):
-    return render(request, 'piazzolebasic.html')
-
-def CamperstopPageView(request):
-    return render(request, 'camper-stop.html')
-
-def ChaletPageView(request):
-    return render(request, 'chalet.html')
-
-def FamigliaPageView(request):
-    return render(request, 'famiglia.html')
-
-def RelaxPageView(request):
-    return render(request, 'relax.html')
-
-def SportPageView(request):
-    return render(request, 'sport.html')
-
-def BiciclettaPageView(request):
-    return render(request, 'bicicletta.html')
+def MountainbikePageView(request):
+    return render(request, 'mountainbike.html')
 
 def TrekkingPageView(request):
     return render(request, 'trekking.html')
 
-def SkiingPageView(request):
-    return render(request, 'skiing.html')
-
-def PrezziPageView(request):
-    return render(request, 'prezzi.html')
+def EventiPageView(request):
+    return render(request, 'eventi.html')
 
 def ContattiPageView(request):
     return render(request, 'contatti.html', {})

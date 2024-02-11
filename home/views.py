4a3f48891f86e5f228057a442c64f39d54e54b4f
Sky3z <skyfallez4316@gmail.com>
2024-02-11 11:20:58 +0100

    fix some, added multilanguage

--- home/views.py
+++ home/views.py
@@ -11,6 +11,9 @@ def HomePageView(request):
 def ServiziPageView(request):
     return render(request, 'servizi.html')
 
+def GalleriaPageView(request):
+    return render(request, 'gallery.html')
+
 def DintorniPageView(request):
     return render(request, 'dintorni.html')
 
@@ -24,7 +27,52 @@ def EventiPageView(request):
     return render(request, 'eventi.html')
 
 def ContattiPageView(request):
-    return render(request, 'contatti.html', {})
+    return render(request, 'contatti.html')
 
-def FaqPageView(request):
-    return render(request, 'faq.html')
+def HomeenPageView(request):
+    return render(request, 'indexen.html')
+
+def ServicesenPageView(request):
+    return render(request, 'servizien.html')
+
+def GalleryenPageView(request):
+    return render(request, 'galleryen.html')
+
+def TrekkingenPageView(request):
+    return render(request, 'trekkingen.html')
+
+def MountainbikeenPageView(request):
+    return render(request, 'mountainbikeen.html')
+
+def DintornienPageView(request):
+    return render(request, 'dintornien.html')
+
+def EventienPageView(request):
+    return render(request, 'eventien.html')
+
+def ContattienPageView(request):
+    return render(request, 'contattien.html')
+
+def HomedePageView(request):
+    return render(request, 'indexde.html')
+
+def ServicesdePageView(request):
+    return render(request, 'servizide.html')
+
+def GallerydePageView(request):
+    return render(request, 'galleryde.html')
+
+def TrekkingdePageView(request):
+    return render(request, 'trekkingde.html')
+
+def MountainbikedePageView(request):
+    return render(request, 'mountainbikede.html')
+
+def DintornidePageView(request):
+    return render(request, 'dintornide.html')
+
+def EventidePageView(request):
+    return render(request, 'eventide.html')
+
+def ContattidePageView(request):
+    return render(request, 'contattide.html')
